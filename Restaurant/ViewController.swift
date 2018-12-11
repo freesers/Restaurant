d5f77df459eac51edead043b5838ebc64543bb3a
Sander de Vries <sanderdv@icloud.com>
2018-12-11 12:59:12 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Sander de Vries on 11/12/2018.
+//  Copyright © 2018 Sander de Vries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
